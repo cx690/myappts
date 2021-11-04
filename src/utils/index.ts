e702889ec36bd21104b02bbedcf23eab5bfd4156
caixue <1256953820@qq.com>
2021-11-04 16:13:34 +0800

    优化打印数据和日志记录

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,6 @@
 import fs from 'fs';
 import path from 'path';
+import { logger } from '../config/logger.js';
 
 /**
  * 获取目录下所有js文件路径
@@ -16,12 +17,22 @@ export async function getPath(dirname: string, dir: string) {
     return urls;
 }
 
-/** 控制台打印 */
-export function clg(msg: any, talk?: (...arsg: any[]) => any | void) {
+export function clg(msg: any, recordLog?: false, talk?: ((...arsg: any[]) => any | void) | undefined): void;
+export function clg(msg: string, recordLog: true, talk?: ((...arsg: any[]) => any | void) | undefined): void;
+/**
+ * 控制台打印与记录日志
+ * @param msg 要打印的数据
+ * @param recordLog 是否记录为日志，默认false，true要求msg为string类型
+ * @param talk 自定义的控制台输出函数
+ */
+export function clg(msg: any, recordLog = false, talk?: (...arsg: any[]) => any | void) {
     if (process.env.NODE_ENV === 'production' || msg === '') return;
     if (talk && typeof talk === 'function') {
         talk(msg);
     } else {
         console.log(msg);/* eslint-disable-line no-console */
+    }
+    if (recordLog) {
+        logger.info(msg);
     }
 }
